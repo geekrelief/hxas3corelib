46b759ddd2afebfacfc89885064cd87760a9709f
Don-Duong Quach <geekrelief@gmail.com>
2008-10-26 01:42:15 -0700

    Added SHA1

--- tests/Main.hx
+++ tests/Main.hx
@@ -1,4 +1,5 @@
 import com.adobe.crypto.MD5Test;
+import com.adobe.crypto.SHA1Test;
 import com.adobe.serialization.json.JSONTest;
 
 class Main
@@ -6,6 +7,7 @@ class Main
     static function main() {
 		var runner = new haxe.unit.TestRunner();
 		runner.add(new MD5Test());
+		runner.add(new SHA1Test());
 		runner.add(new JSONTest());
 		runner.run();
 	}
